906d00d0206cb3e1b4666c120be7971d97eaa5f6
Unknown <jayden.m.maalouf@gmail.com>
2018-01-12 17:18:40 +1100

    Auto stash before merge of "master" and "origin/master"
    Added random manager and raffle handler

---
+++ DallarBot/Classes/RandomManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DallarBot.Classes
+{
+    public class RandomManager
+    {
+        public RandomManager(Int64 min, Int64 max)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.random.org/json-rpc/1/invoke");
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
+
+            JObject param = new JObject();
+            param["apiKey"] = "fbef3728-6fa6-41d3-bd3e-0df830160a2e";
+            param["n"] = 1;
+            param["min"] = min;
+            param["max"] = max;
+
+            JObject sendObject = new JObject();
+            sendObject["jsonrpc"] = "2.0";
+            sendObject["id"] = "1";
+            sendObject["method"] = "generateIntegers";
+            sendObject.Add(new JProperty("params", param));
+
+            string serializedObject = JsonConvert.SerializeObject(sendObject);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(serializedObject);
+            webRequest.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = webRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                using (Stream str = webResponse.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(str))
+                    {
+                        string rte = sr.ReadToEnd();
+                        var obj = JsonConvert.DeserializeObject<JObject>(rte);
+                    }
+                }
+            }
+        }
+    }
+}
