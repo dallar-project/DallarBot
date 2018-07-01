d502b057074b51ef77ef12e301a43f0a74a33182
Matin <MatinGitHub@gmail.com>
2018-07-01 01:08:03 -0700

    Added Dallar Conversation Rate Command
    Pulls data from digitalprice to determine the Dallar value.
    
    !dal-btc
    Includes the last price, 24 hour low, 24 hour high, % change, and 24 hour volume.
    
    !dal-usd
    Includes the last price of dallar in USD.

--- DallarBot/Modules/MiscCommander.cs
+++ DallarBot/Modules/MiscCommander.cs
@@ -71,5 +71,58 @@ namespace DallarBot.Modules
                 await Context.Channel.SendMessageAsync(":regional_indicator_g: :regional_indicator_i: :regional_indicator_v: :regional_indicator_e: :a: :regional_indicator_f: :regional_indicator_u: :regional_indicator_c: :regional_indicator_k:");
             }
         }
+
+        [Command("dal-btc")]
+        public async Task DallarToBTC()
+        {
+            var user = Context.User as SocketGuildUser;
+
+            var client = new WebClient();
+            var jsonString = await client.DownloadStringTaskAsync("https://digitalprice.io/markets/get-currency-summary?currency=BALANCE_COIN_BITCOIN");
+
+            var dalConverter = DallarSerialization.FromJson(jsonString);
+
+            for (int i = 0; i < dalConverter.Length; i++)
+            {
+                if (dalConverter[i].MiniCurrency == "dal-btc")
+                {
+                    Console.WriteLine(Environment.NewLine +
+                        "Dallar Price (DAL / BTC): " + dalConverter[i].Price.ToString() + Environment.NewLine +
+                        "24 Hour Low: " + dalConverter[i].Low.ToString() + Environment.NewLine +
+                        "24 Hour High: " + dalConverter[i].High.ToString() + Environment.NewLine +
+                        "% Change: " + dalConverter[i].PriceChange + Environment.NewLine +
+                        "24 Hour Volume: " + dalConverter[i].VolumeMarket.ToString());
+
+                    await Context.Channel.SendMessageAsync("Dallar Price (DAL / BTC): " + (decimal.Round(dalConverter[i].Price, 9, MidpointRounding.AwayFromZero) + Environment.NewLine +
+                                "24 Hour Low: " + (dalConverter[i].Low.ToString() + Environment.NewLine +
+                                "24 Hour High: " + dalConverter[i].High.ToString() + Environment.NewLine +
+                                "% Change: " + dalConverter[i].PriceChange + Environment.NewLine +
+                                "24 Hour Volume: " + dalConverter[i].VolumeMarket.ToString())));
+                }
+            }
+        }
+
+        [Command("dal-usd")]
+        public async Task DallarToUSD()
+        {
+            var user = Context.User as SocketGuildUser;
+
+            var client = new WebClient();
+            var jsonString = await client.DownloadStringTaskAsync("https://digitalprice.io/markets/get-currency-summary?currency=BALANCE_COIN_BITCOIN");
+            var btcPrice = await client.DownloadStringTaskAsync("https://blockchain.info/tobtc?currency=USD&value=1");
+
+            var dalConverter = DallarSerialization.FromJson(jsonString);
+
+            for (int i = 0; i < dalConverter.Length; i++)
+            {
+                if (dalConverter[i].MiniCurrency == "dal-btc")
+                {
+                    Console.WriteLine(Environment.NewLine +
+                        "DAL / USD: $" + decimal.Round((dalConverter[i].Price / Convert.ToDecimal(btcPrice)), 8, MidpointRounding.AwayFromZero));
+
+                    await Context.Channel.SendMessageAsync("DAL / USD: $" + decimal.Round((dalConverter[i].Price / Convert.ToDecimal(btcPrice)), 9, MidpointRounding.AwayFromZero));
+                }
+            }
+        }
     }
 }
