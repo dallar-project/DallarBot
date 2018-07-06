03437fb41ee10299e8f03c426d48538ce852c723
Michael Allar <allar@michaelallar.com>
2018-07-06 16:06:49 -0700

    Stable and back in working order.

---
+++ DallarBot/Services/YoMommaJokeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace DallarBot.Services
+{
+    public class YoMommaJokeService
+    {
+        protected static string[] YoMommaJokes = { };
+        protected static Random YoMommaRandom = new Random();
+
+        public YoMommaJokeService()
+        {
+            PopulateMommaJokes();
+        }
+
+        public bool PopulateMommaJokes()
+        {
+            if (YoMommaJokes.Length == 0)
+            {
+                YoMommaJokes = File.ReadAllLines("momma.txt");
+            }
+
+            return YoMommaJokes.Length > 1;
+        }
+
+        public string GetRandomYoMommaJoke()
+        {
+            PopulateMommaJokes();
+            return YoMommaJokes[YoMommaRandom.Next(YoMommaJokes.Length - 1)];
+        }
+    }
+}
